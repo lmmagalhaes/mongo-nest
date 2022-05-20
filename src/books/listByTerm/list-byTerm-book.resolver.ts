2b82c05b5dcafaffdcf09bb37725d78255271347
lmmagalhaes <leo.maga17@gmail.com>
2022-05-20 10:17:10 -0300

    add list by term book service

---
+++ src/books/listByTerm/list-byTerm-book.resolver.ts
@@ -0,0 +1,15 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { BookDto } from '../dto/book.dto';
+import { FindBookByTermInput } from '../inputs/book.input';
+import ListByTermBookService from './list-byTerm-book.service';
+
+@Resolver()
+export class ListByTermBookResolver {
+  constructor(private booksService: ListByTermBookService) {}
+
+  // Eu, como usuário, desejo listar os livros cadastrados filtrando por uma palavra chave;
+  @Query(() => [BookDto])
+  async findByTerm(@Args('input') input: FindBookByTermInput) {
+    return this.booksService.findByTerm(input.term);
+  }
+}
---
+++ src/books/listByTerm/list-byTerm-book.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Books } from '../books.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export default class ListByTermBookService {
+  constructor(
+    @InjectModel(Books.name)
+    private bookModel: Model<Books>,
+  ) {}
+
+  async findByTerm(title: string): Promise<Books[]> {
+    return this.bookModel.find({ title: { $regex: /^input.term/ } });
+  }
+}
